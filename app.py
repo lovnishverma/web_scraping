a9fe80b07e55701ac7a4ecc9364de7ce3804333d
Glitch (glitch-hello-website) <none>
2023-07-14 12:09:32 +0000

    🎪🍝 Checkpoint ./templates/page.html:57629255/191 ./templates/page1.html:57629255/119 ./templates/page2.html:57629255/119 ./templates/page3.html:57629255/119 ./app.py:57629255/55 ./requirements.txt:57629255/13 ./templates/index.html:57629255/409 ./requirements.tx:57629255/1

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import *
+from datetime import datetime
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("index.html")
+  return render_template("index.html", now=datetime.now())
 
 @app.route('/page')
 def page():
   return render_template("page.html")
 
-@app.route('/page')
+@app.route('/page1')
 def page1():
   return render_template("page1.html")
 
-@app.route('/page')
+@app.route('/page2')
 def page2():
   return render_template("page2.html")
 
-@app.route('/page')
+@app.route('/page3')
 def page3():
   return render_template("page3.html")
 
