0f254ee471f930ef227a199c02cc588fe3a428b0
Glitch (glitch-hello-website) <none>
2023-07-14 11:57:32 +0000

    🍱🛳 Checkpoint ./app.py:57629255/219

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def home():
 def page():
   return render_template("page.html")
 
+@app.route('/page')
+def page1():
+  return render_template("page1.html")
+
+@app.route('/page')
+def page2():
+  return render_template("page2.html")
+
+@app.route('/page')
+def page3():
+  return render_template("page3.html")
+
 if __name__ == '__main__':
   app.run()
