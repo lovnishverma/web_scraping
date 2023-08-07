f7a0f0bb1885c8d280ce1ab382d8166ef5fd4357
Glitch (glitch-hello-website) <none>
2023-08-07 16:42:41 +0000

    🍃📹 Checkpoint ./app.py:57629255/7386 ./requirements.txt:57629255/1 ./templates/index.html:57629255/1853

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response, render_template
 from PIL import Image
 import requests
 from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
@@ -12,29 +13,28 @@ def index():
 @app.route("/rotate-image", methods=['GET'])
 def rotate_image():
     try:
-    degree = int(request.args.get('degree', 0))
-    url = request.args.get('url')
-    print("Received URL:", url)  # Print URL for debugging
+        degree = int(request.args.get('degree', 0))
+        url = request.args.get('url')
 
-  
-# @app.route("/rotate-image", methods=['GET'])
-# def rotate_image():
-#     try:
-#         degree = int(request.args.get('degree', 0))
-#         url = request.args.get('url')
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
 
-#         response = requests.get(url)
-#         img = Image.open(BytesIO(response.content))
+        rotated_img = img.rotate(degree, expand=True)
 
-#         rotated_img = img.rotate(degree, expand=True)
+        # Convert RGBA image to RGB if necessary
+        if rotated_img.mode == 'RGBA':
+            rotated_img = rotated_img.convert('RGB')
 
-#         output = BytesIO()
-#         rotated_img.save(output, format='JPEG')
-#         output.seek(0)
+        output = BytesIO()
+        rotated_img.save(output, format='JPEG')
+        output.seek(0)
 
-#         return Response(output, content_type='image/jpeg')
-#     except Exception as e:
-#         return str(e), 400
+        rotated_image_base64 = base64.b64encode(output.read()).decode('utf-8')
+
+        return render_template("index.html", rotated_image=rotated_image_base64)
+
+    except Exception as e:
+        return str(e), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
