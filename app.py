332870d199a4e0cc3f9ba577e399644b0694d57f
Glitch (flasktestapp) <none>
2023-08-31 16:33:14 +0000

    🐃🎻 Checkpoint ./app.py:57629255/2679 ./templates/index.html:57629255/2044 ./requirements.txt:57629255/1

--- app.py
+++ app.py
@@ -1,41 +1,46 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 # Handle the default web page request
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    json_data = scrape()
-    return render_template("index.html", headings=json_data['headings'])
+    if request.method == 'POST':
+        url = request.form['url']
+        tag = request.form['tag']
+        if url and tag:
+            json_data = scrape(url, tag)
+            return render_template("index.html", headings=json_data['headings'], url=url, tag=tag)
+    return render_template("index.html")
 
 # Function to scrape a page
-def scrape():
-    # Specify the URL of the page we want to scrape
-    # url = "https://www.thinkcreatelearn.co.uk/resources/node/web-scraping/sample1.html"
-    url = "https://nielitropar.000webhostapp.com/"
-    
-    # Make the HTTP request to the URL to get the data
-    response = requests.get(url)
-    data = response.content
-    
-    # Load the HTML into BeautifulSoup web scraper
-    soup = BeautifulSoup(data, 'html.parser')
-    
-    # Create a dictionary to store the scraped data
-    json_data = {'headings': []}
-    
-    # Search for the elements we want
-    selection = soup.find_all('h3')
-    
-    # Add the elements to the dictionary
-    for el in selection:
-        text = el.get_text()
-        json_data['headings'].append(text)
-    
-    return json_data
+def scrape(url, tag):
+    try:
+        # Make the HTTP request to the URL to get the data
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.content
+
+        # Load the HTML into BeautifulSoup web scraper
+        soup = BeautifulSoup(data, 'html.parser')
+
+        # Create a dictionary to store the scraped data
+        json_data = {'headings': []}
+
+        # Search for the elements of the specified tag
+        selection = soup.find_all(tag)
+
+        # Add the elements to the dictionary
+        for el in selection:
+            text = el.get_text()
+            json_data['headings'].append(text)
+
+        return json_data
+    except Exception as e:
+        return {'error': str(e)}
 
 # Start the app running on the web server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
